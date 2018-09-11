535527d621719d7335c558b6cbf4332a9d6287b0
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-09-11 16:57:05 +0200

    Reworked back navigation with new style and behaviour

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
     return (
       <div>
         <div id="header">
-          <Link to="/">
-            <button id="back">back</button>
-          </Link>
+          <NavLink to="/" exact activeClassName="active" >
+            <button className="back"> <img src="http://res.cloudinary.com/nahov91/image/upload/f_auto,fl_sanitize,q_auto/v1536674640/icons8-back-50.png" alt="navigate back"/> <span> Back</span></button>
+          </NavLink>
         </div>
       </div>
     );
