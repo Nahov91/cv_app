12e764a9994904b2c89096b1c165bb72a695d8c9
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-09-11 23:04:22 +0200

    Fix: styling of details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -24,20 +24,29 @@ export default class Details extends Component {
   render() {
     const { school } = this.state;
     return (
-      <div>
+      <div id="details">
         <Backdrop />
-        <h1 className="schoolDetailName"> {school.schoolName} </h1>
-        <h5 className="courseDetailName"> {school.courseName} </h5>
-        <h5 className="courseDetailDuration"> {school.courseDuration} </h5>
         <img
           className="schoolDetailImage"
           src={school.schoolImage}
           alt={school.schoolName}
         />
-        <p className="shortDetailDescription"> {school.shortDescription} </p>
-        <p className="problemsToSolve"> {school.problemsToSolve} </p>
-        <p className="detailTechnologies"> {school.technologies} </p>
-        <p className="detailTeachers"> {school.teachers} </p>
+        <div className="detailsWrapper">
+          <div className="introductionWrapper">
+            <h1 className="courseDetailName"> {school.courseName} </h1>
+            <h5 className="schoolDetailName"> {school.schoolName} </h5>
+            <h5 className="courseDetailDuration"> {school.courseDuration} </h5>
+            <p className="shortDetailDescription">
+              {" "}
+              {school.shortDescription}{" "}
+            </p>
+          </div>
+          <p className="problemsToSolve"> {school.problemsToSolve} </p>
+          <div className="otherInfo">
+            <p className="detailTechnologies"> {school.technologies} </p>
+            <p className="detailTeachers"> {school.teachers} </p>
+          </div>
+        </div>
       </div>
     );
   }
